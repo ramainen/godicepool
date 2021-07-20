c0b9f50a03fecb07b2c1adcc785d500fcbe8d6c3
ramainen <ainu.sky@gmail.com>
2021-07-20 23:22:22 +0300

    WARM works in JS

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"syscall/js"
 	"time"
 )
 
@@ -13,6 +14,8 @@ func main() {
 	fmt.Println(result.CritsCount)
 	fmt.Println(result.NonCritsCount)
 	fmt.Println(result.Rolls)
+
+	js.Global().Set("MakeAttackSeries", WASMMakeAttackSeries())
 
 	body1 := NewBody()
 	body1.Defence = 4
@@ -31,5 +34,30 @@ func main() {
 	fmt.Println(attackSeriesResult)
 
 	//fmt.Println(MakeAttackRound(weapon1, body1, 2))
+	select {}
+}
 
+func WASMMakeAttackSeries() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		body1 := NewBody()
+		body1.Defence = 4
+		body1.Wounds = 7
+		body1.FNP = 5
+		body1.Save = 5
+
+		weapon1 := NewWeapon()
+		weapon1.BS = 3
+		weapon1.Attacks = 4
+		weapon1.MW = 3
+		weapon1.NormalDamage = 2
+		weapon1.CriticalDamage = 3
+
+		attackSeriesResult := MakeAttackSeries(1000, weapon1, body1, 2)
+		return map[string]interface{}{
+			"hello":  "world",
+			"answer": 42,
+			"killed": attackSeriesResult.Killed,
+		}
+
+	})
 }
